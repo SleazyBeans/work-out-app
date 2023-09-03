6ca5a5d10e18596cf5bfe0f4533196fd7446ab5d
SleazyBeans <127462851+SleazyBeans@users.noreply.github.com>
2023-09-03 18:21:48 -0230

    fixed code it works again

--- html-boilerplate/script.js
+++ html-boilerplate/script.js
@@ -5,7 +5,7 @@ const push = ['bench press', 'overhead press', 'incline dumbbell press', 'tricep
 
     const workoutSelect = document.getElementById('ddl');
     const routineSelect = document.getElementById('ddl2');
-    workoutSelect.addEventListener('select',() => {
+    workoutSelect.addEventListener('change',() => {
         configureDropDownLists(workoutSelect, routineSelect);});
     
 const routines = new Map();
@@ -13,12 +13,19 @@ routines.set('push', push);
 routines.set('pull', pull);
 routines.set('legs', legs);
 
+function createOption(selectBox, optionName){
+    const option = document.createElement("option");
+    option.text = option.value = optionName;
+    selectBox.appendChild(option);
+    
+}
 
 
 function configureDropDownLists(ddl, ddl2){  
     ddl2.options.length = 0;
 
     const routine = routines.get(ddl.value)
+    console.log(routine)
     for (i = 0; i < routine.length; i++) {
         createOption(ddl2, routine[i], routine[i])
     }
