a6c4395f88b67500455a0fc92ae6811e9c8caf95
SleazyBeans <127462851+SleazyBeans@users.noreply.github.com>
2023-09-03 14:00:56 -0230

    made changes broke the code no longer works as it did, also added new html and css for next part of project

--- html-boilerplate/script.js
+++ html-boilerplate/script.js
@@ -1,50 +1,26 @@
-function configureDropDownLists(ddl1, ddl2){  
-    var push = ['bench press', 'overhead press', 'incline dumbbell press', 'triceps push downs', 'lateral raises', 'overhead tricep extensions'];
-    var pull = ['deadlift', 'pull-up','hammer curls', 'dumbbell curls', 'seated cable row', 'facepulls'];
-    var legs = ['dumbbell squat', 'leg press', 'leg curls', 'calf raises'];
-    var howvar = ['how you gain']
-    switch (ddl1.value){
-        case 'routines':
-            ddl2.options.length = 0;
-            for (i = 0; i < routines.length; i++) {
-                createOption(ddl2, routines[i], routines[i]);
-            }
-            break;
-        case 'push':
-            ddl2.options.length = 0;
-            for (i = 0; i< push.length; i++){
-                createOption(ddl2, push[i], push[i]);
-            }
-            break;
-        case 'pull':
-            ddl2.options.length = 0;
-            for (i = 0; i < pull.length; i++){
-                createOption(ddl2, pull[i], pull[i]);
-            }
-            break;
-        case 'legs':
-            ddl2.options.length = 0;
-            for (i=0; i < legs.length; i++){
-                createOption(ddl2, legs[i], legs[i]);
-            }
-            break;
-        case 'howyougain' :
-            ddl2.options.length = 0;
-            for (i = 0; i < howvar.length; i++) {
-                createOption(ddl2, howvar[i], howvar[i]);
-            }
-            break;
-        default:
-            ddl2.options.length = 0;
-            break;
-        }
+const push = ['bench press', 'overhead press', 'incline dumbbell press', 'triceps push downs', 'lateral raises', 'overhead tricep extensions'];
+    const pull = ['deadlift', 'pull-up','hammer curls', 'dumbbell curls', 'seated cable row', 'facepulls'];
+    const legs = ['dumbbell squat', 'leg press', 'leg curls', 'calf raises'];
+    const howvar = ['how you gain']
 
-}
+    const workoutSelect = document.getElementById('ddl');
+    const routineSelect = document.getElementById('ddl2');
+    workoutSelect.addEventListener('select',() => {
+        configureDropDownLists(workoutSelect, routineSelect);});
+    
+const routines = new Map();
+routines.set('push', push);
+routines.set('pull', pull);
+routines.set('legs', legs);
 
-function createOption(ddl, text, value) {
-    var opt = document.createElement('option');
-    opt.value = value;
-    opt.text = text;
-    ddl.options.add(opt);
+
+
+function configureDropDownLists(ddl, ddl2){  
+    ddl2.options.length = 0;
+
+    const routine = routines.get(ddl.value)
+    for (i = 0; i < routine.length; i++) {
+        createOption(ddl2, routine[i], routine[i])
+    }
 }
 
